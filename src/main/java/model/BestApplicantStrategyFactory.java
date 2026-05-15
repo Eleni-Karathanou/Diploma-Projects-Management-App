a6a81c28e2db051023e655ff5298da5c05e24d1e
Eleni-Karathanou <elenk9421@gmail.com>
2026-05-15 13:31:21 +0300

    Update BestApplicantStrategyFactory.java

--- src/main/java/model/BestApplicantStrategyFactory.java
+++ src/main/java/model/BestApplicantStrategyFactory.java
@@ -2,13 +2,13 @@ package model;
 
 import org.springframework.stereotype.Component;
 
-@Component // Το κάνουμε Component για να μπορεί το Spring να το κάνει @Autowired στα Services σου
+@Component //Component για να μπορεί το Spring να το κάνει @Autowired στα Services
 public class BestApplicantStrategyFactory {
 
     public BestApplicantStrategyFactory() {
     }
     
-    // Η μόνη δουλειά του Factory: να επιστρέφει το σωστό αντικείμενο Στρατηγικής
+    // επιστρέφει το σωστό αντικείμενο Στρατηγικής
     public BestApplicantStrategy createStrategy(String strategyType) {
         
         if (strategyType == null) {
@@ -22,7 +22,6 @@ public class BestApplicantStrategyFactory {
             case "fewestcourses":
                 return new FewestCoursesStrategy();
                 
-            // Μπορείς να προσθέσεις κι άλλες στρατηγικές εδώ στο μέλλον (π.χ. "random", "threshold")
                 
             default:
                 throw new IllegalArgumentException("Unknown strategy type: " + strategyType);
